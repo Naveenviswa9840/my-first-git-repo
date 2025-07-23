a8460adc790bfd9ecd950c0cd631b2c6936dbf04
Naveen Viswa <naveenviswa1626@gmail.com>
2025-07-23 14:32:30 +0530

    Updated files in React project

---
+++ src/course.jsx
@@ -0,0 +1,18 @@
+const course1 = "HTML"
+
+function Course(){
+
+
+return(
+
+    <div className="card">
+        <img src="" alt="" />
+        <h3>{corse1}</h3>
+        <p>This is my First Course</p>
+        <p>This is my First Course</p>
+    </div>
+
+);
+
+}
+export default Course
