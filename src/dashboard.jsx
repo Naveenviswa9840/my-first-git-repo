20ba9f711acc02cdf3863f460c8bbab34b15c16a
Naveen Viswa <naveenviswa1626@gmail.com>
2025-07-23 14:43:02 +0530

    dashboard

---
+++ src/dashboard.jsx
@@ -0,0 +1,17 @@
+
+function Dashboard(){
+
+
+return(
+
+    <div className="card">
+        <img src="" alt="" />
+        <h3>{corse1}</h3>
+        <p>This is my First Course</p>
+        <p>This is my First Course</p>
+    </div>
+
+);
+
+}
+export default Dashboard
